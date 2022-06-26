7e53afa56515dd23c0f12bc46ef40117a29f3a22
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-06-26 08:04:20 +0100

    Added basic UI componenets

--- app/src/main/java/android/banananabandit/figuresupdate/MainActivity.kt
+++ app/src/main/java/android/banananabandit/figuresupdate/MainActivity.kt
@@ -2,10 +2,27 @@ package android.banananabandit.figuresupdate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
+    var figureInput : EditText? = null
+    var numberOfCustInput : EditText? = null
+
+    var switchFinal : ToggleButton? = null
+    var switchUpdate : ToggleButton? = null
+
+    var updateButton : Button? = null
+
+    var figureResult : TextView? = null
+    var atvResult : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //instantiate all
     }
 }
