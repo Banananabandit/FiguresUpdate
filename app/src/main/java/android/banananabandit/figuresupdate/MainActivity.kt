8a96b74fc7f9088e249cd361d8d3c04fb40fbe11
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-06-28 06:26:40 +0100

    Refactoring code

--- app/src/main/java/android/banananabandit/figuresupdate/MainActivity.kt
+++ app/src/main/java/android/banananabandit/figuresupdate/MainActivity.kt
@@ -1,10 +1,8 @@
 package android.banananabandit.figuresupdate
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.*
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.button.MaterialButtonToggleGroup
@@ -15,47 +13,20 @@ import kotlin.math.roundToInt
 class MainActivity : AppCompatActivity() {
     var figureInput : EditText? = null
     private var numberOfCustInput : EditText? = null
-
-
-    var switchFinal : Button? = null
-    var switchUpdate : Button? = null
-
-    var shareButton : Button? = null
-
-    var figureResult : TextView? = null
-    var atvResult : TextView? = null
-
-    private var result : Double? = null
-    private var roundResult : Int? = null
+    private var switchFinal : Button? = null
+    private var switchUpdate : Button? = null
+    private var shareButton : Button? = null
+    private var figureResult : TextView? = null
+    private var atvResult : TextView? = null
+    private var result : Int? = null
     private var atvInput : Double? = null
     private var finalAtvFigure : String? = null
     private var numberOfCustomers : String? = null
-
     private var resultMessage : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var toggleButtonGroup = findViewById<MaterialButtonToggleGroup>(R.id.toggleButtonGroup)
-        toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
-
-            when (checkedId) {
-                R.id.switchUpdate -> {
-                    if (isChecked) {
-                        resultMessage = "Update £$roundResult $numberOfCustomers trans $finalAtvFigure ATV"
-                        Log.d(TAG, "onCreate: switched to Update $resultMessage")
-                    }
-                }
-                R.id.switchFinal -> {
-                    if (isChecked) {
-                        resultMessage = "Finished today on £$roundResult $numberOfCustomers trans $finalAtvFigure ATV"
-                        Log.d(TAG, "onCreate: switched to Final Message $resultMessage")
-                    }
-                }
-                else -> resultMessage = "Can you please choose"
-            }
-    }
 
         figureInput = findViewById(R.id.figureInput)
         numberOfCustInput = findViewById(R.id.numberOfCust)
@@ -65,17 +36,34 @@ class MainActivity : AppCompatActivity() {
         figureResult = findViewById(R.id.figureResult)
         atvResult = findViewById(R.id.atvResult)
 
+        val toggleButtonGroup = findViewById<MaterialButtonToggleGroup>(R.id.toggleButtonGroup)
+        toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
+
+            when (checkedId) {
+                R.id.switchUpdate -> {
+                    if (isChecked) {
+                        resultMessage = "Update £$result $numberOfCustomers trans $finalAtvFigure ATV"
+                    }
+                }
+                R.id.switchFinal -> {
+                    if (isChecked) {
+                        resultMessage = "Finished today on £$result $numberOfCustomers trans $finalAtvFigure ATV"
+                    }
+                }
+                else -> resultMessage = "Can you please choose"
+            }
+    }
+
         figureInput?.addTextChangedListener {
             if (figureInput?.text?.isNotEmpty() == true) {
-                result = (figureInput?.text.toString().toDouble() / 1.2)
-                roundResult = result?.roundToInt()
-                figureResult?.text = roundResult.toString()
+                result = (figureInput?.text.toString().toDouble() / 1.2).roundToInt()
+                figureResult?.text = result.toString()
             }
         }
 
         numberOfCustInput?.addTextChangedListener {
             if (numberOfCustInput?.text?.isNotEmpty() == true) {
-                atvInput = roundResult?.div(numberOfCustInput?.text.toString().toDouble())
+                atvInput = result?.div(numberOfCustInput?.text.toString().toDouble())
                 val df = DecimalFormat("#.##")
                 df.roundingMode = RoundingMode.CEILING
                 numberOfCustomers = numberOfCustInput?.text.toString()
@@ -85,11 +73,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         shareButton?.setOnClickListener {
-            whatsApp()
+            whatsAppResultShare()
         }
-
     }
-    private fun whatsApp() {
+    private fun whatsAppResultShare() {
         val intent = Intent()
         intent.action = Intent.ACTION_SEND
         intent.putExtra(Intent.EXTRA_TEXT, resultMessage)
@@ -97,5 +84,4 @@ class MainActivity : AppCompatActivity() {
         intent.setPackage("com.whatsapp")
         startActivity(intent)
     }
-
 }
