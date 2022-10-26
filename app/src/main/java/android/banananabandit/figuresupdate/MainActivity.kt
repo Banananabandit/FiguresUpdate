07cbe70710e713d2949e3a5c0ff2f45ec8eb3751
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-10-26 06:02:42 +0100

    Adding reset button

--- app/src/main/java/android/banananabandit/figuresupdate/MainActivity.kt
+++ app/src/main/java/android/banananabandit/figuresupdate/MainActivity.kt
@@ -1,6 +1,7 @@
 package android.banananabandit.figuresupdate
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -11,18 +12,20 @@ import java.text.DecimalFormat
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
-    var figureInput : EditText? = null
-    private var numberOfCustInput : EditText? = null
-    private var switchFinal : Button? = null
-    private var switchUpdate : Button? = null
-    private var shareButton : Button? = null
-    private var figureResult : TextView? = null
-    private var atvResult : TextView? = null
-    private var result : Int? = null
-    private var atvInput : Double? = null
-    private var finalAtvFigure : String? = null
-    private var numberOfCustomers : String? = null
-    private var resultMessage : String? = null
+    var figureInput: EditText? = null
+    private var numberOfCustInput: EditText? = null
+    private var switchFinal: Button? = null
+    private var switchUpdate: Button? = null
+    private var shareButton: Button? = null
+    private var figureResult: TextView? = null
+    private var atvResult: TextView? = null
+    private var result: Int? = null
+    private var atvInput: Double? = null
+    private var finalAtvFigure: String? = null
+    private var numberOfCustomers: String? = null
+    private var resultMessage: String? = null
+    private var toggleButtonGroup: MaterialButtonToggleGroup? = null
+    private var resetFiguresButton: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,26 +36,29 @@ class MainActivity : AppCompatActivity() {
         switchFinal = findViewById(R.id.switchFinal)
         switchUpdate = findViewById(R.id.switchUpdate)
         shareButton = findViewById(R.id.shareButton)
+        resetFiguresButton = findViewById(R.id.figuresResetButton)
         figureResult = findViewById(R.id.figureResult)
         atvResult = findViewById(R.id.atvResult)
 
-        val toggleButtonGroup = findViewById<MaterialButtonToggleGroup>(R.id.toggleButtonGroup)
-        toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
+        toggleButtonGroup = findViewById(R.id.toggleButtonGroup)
+        toggleButtonGroup?.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
 
             when (checkedId) {
                 R.id.switchUpdate -> {
                     if (isChecked) {
-                        resultMessage = "Update £$result $numberOfCustomers trans £$finalAtvFigure ATV"
+                        resultMessage =
+                            "Update £$result $numberOfCustomers trans £$finalAtvFigure ATV"
                     }
                 }
                 R.id.switchFinal -> {
                     if (isChecked) {
-                        resultMessage = "Finished today on £$result $numberOfCustomers trans £$finalAtvFigure ATV"
+                        resultMessage =
+                            "Finished today on £$result $numberOfCustomers trans £$finalAtvFigure ATV"
                     }
                 }
                 else -> resultMessage = "Can you please choose"
             }
-    }
+        }
 
         figureInput?.addTextChangedListener {
             if (figureInput?.text?.isNotEmpty() == true) {
@@ -72,11 +78,26 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        resetFiguresButton?.setOnClickListener {
+            toggleButtonGroup?.clearChecked()
+
+            numberOfCustInput?.text?.clear()
+            figureInput?.text?.clear()
+            figureInput?.requestFocus()
+
+            figureResult?.text = "Result:"
+            atvResult?.text = "ATV:"
+        }
+
         shareButton?.setOnClickListener {
             whatsAppResultShare()
+            Toast.makeText(this, "$result $numberOfCustomers $finalAtvFigure", Toast.LENGTH_SHORT)
+                .show()
         }
     }
+
     private fun whatsAppResultShare() {
+
         val intent = Intent()
         intent.action = Intent.ACTION_SEND
         intent.putExtra(Intent.EXTRA_TEXT, resultMessage)
@@ -84,4 +105,6 @@ class MainActivity : AppCompatActivity() {
         intent.setPackage("com.whatsapp")
         startActivity(intent)
     }
+
+
 }
