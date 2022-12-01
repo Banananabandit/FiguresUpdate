e91099590bac4d8f5acc61afb4995e4f8094862c
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-12-01 09:56:07 +0000

    Added nav_menu layout and a WeeklyFiguresFragment

---
+++ app/src/main/java/android/banananabandit/figuresupdate/WeeklyUpdateFragment.kt
@@ -0,0 +1,15 @@
+package android.banananabandit.figuresupdate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WeeklyUpdateFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_weekly_update, container, false)
+    }
+}
