0e34f249248d0880d470379bff14373cb6245e4f
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-12-19 08:38:12 +0000

    Adding FAB

--- app/src/main/java/android/banananabandit/figuresupdate/WeeklyUpdateFragment.kt
+++ app/src/main/java/android/banananabandit/figuresupdate/WeeklyUpdateFragment.kt
@@ -7,10 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 
 class WeeklyUpdateFragment : Fragment() {
     private var _binding : FragmentWeeklyUpdateBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentWeeklyUpdateBinding.inflate(inflater, container, false)
@@ -18,6 +20,13 @@ class WeeklyUpdateFragment : Fragment() {
         val adapter = TargetNumbersAdapter(FinancialWeek.generateListOfWeeks(20))
         binding.rvWeekTargets.adapter = adapter
         binding.rvWeekTargets.layoutManager = LinearLayoutManager(context)
+
+        val fab = binding.fab
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
 
         return binding.root
     }
